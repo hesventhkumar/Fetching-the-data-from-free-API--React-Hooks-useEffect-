004868d54221186c52d4545b3438a634c62f5b2e
hesventhkumar <hesventh@gmail.com>
2024-08-17 12:04:17 +0530

    initailize

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
+// Use Effect
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { useState,useEffect } from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function ExampleRender(){
+//     const [count,setCount] = useState(0);
+
+//     useEffect(()=>{
+//     setTimeout(() =>{
+//       if(count<10)
+//         setCount(count =>count+1)
+//     },1000)
+// }
+// );
+
+// return(
+//     <h1>Website Counting details {count} times</h1>
+// )
+// }
+
+// ReactDOM.render(<ExampleRender />,document.getElementById("root"));
+function Userdemo(){
+    const [users,setUsers] = useState([]);
+    const [loading,setLoading] = useState(true);
+
+useEffect(()=>{
+fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data =>{
+      setUsers(data);
+      setLoading(false);
+      })
+    .catch (error =>{
+        console.error("if it is not loading will be error message");
+
+        setLoading(false);
+    });
+},[]);
+    if(loading){
+        return <p>Loading..</p>;
+    }
+
+    return(
+        <div>
+        <h1>List out the Users in API</h1>
+        <ul>
+            {users.map(user => (
+                <li key={user.id}>{user.name},{user.email}</li>
+            ))}
+        </ul>
+        </div>
+    );
+}
+ReactDOM.render(<Userdemo />,document.getElementById("root")) ;
